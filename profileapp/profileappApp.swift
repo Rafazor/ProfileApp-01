56163330f544b2fcfb9535993d86e5abf06b1557
Călin Rafa <calin.rafa@alexishr.com>
2023-03-16 22:17:22 +0200

    Initial Commit

---
+++ profileapp/profileappApp.swift
@@ -0,0 +1,17 @@
+//
+//  profileappApp.swift
+//  profileapp
+//
+//  Created by Calin Rafa on 16.03.2023.
+//
+
+import SwiftUI
+
+@main
+struct profileappApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
